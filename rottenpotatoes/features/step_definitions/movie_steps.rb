d69ff1b6074ec60640714a95f36a974e83e47fe5
Antoni Romero Blanch <aromerob@uncc.edu>
2016-11-01 15:40:26 +0000

    First commit

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,11 @@
-# Add a declarative step here for populating the DB with movies.
-
+## --- Part 1: Create a declarative scenario step for adding movies --- ##
+# Add a declarative step here for populating the DB with movies
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.should match /#{e1}.*#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +25,22 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.delete!("\"")
+  if uncheck.nil?
+    rating_list.split(',').each do |field|
+      check("ratings["+field.strip+"]")
+    end
+  else
+    rating_list.split(',').each do |field|
+      uncheck("ratings["+field.strip+"]")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  within("tbody") do
+    rows = all("tr").count
+    expect(rows).to eq 10
+  end
 end
